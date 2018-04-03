d383547cf0c2a65e3389f18b81f7e0e12fe1c0b4
scottbishop <scottbishop70@gmail.com>
2018-04-03 09:55:56 -0700

    System tests are running

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV['HEADLESS']
+    puts 'System tests running in headless mode...'
     driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
         desired_capabilities: {
             chromeOptions: {
