88d08003e9b2d39e442a3c4b9219058d2b7a9611
scottbishop <scottbishop70@gmail.com>
2018-04-03 09:00:39 -0700

    Setup chrome and chromedriver with docker-compose to run system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV['HEADLESS']
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+        desired_capabilities: {
+            chromeOptions: {
+                args: %w[headless disable-gpu no-sandbox]
+            },
+        },
+    }
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 end
